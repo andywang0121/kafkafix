9263e27d4dbacc2e84805b9a8c37c470a3bf61c1
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-08-12 19:14:34 -0400

    add eye icon to redirect to see the partition events for a topic

--- client/src/containers/HomeScreen/TopicsTable/CollapsibleTable.tsx
+++ client/src/containers/HomeScreen/TopicsTable/CollapsibleTable.tsx
@@ -1,77 +1,88 @@
 import React, { FC, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { CollapsibleTableHeaders } from './CollapsibleTableHeaders';
 import {
-	DataEntries,
-	partitionDataEntries,
+  DataEntries,
+  partitionDataEntries,
 } from '../../../state/reducers/kafkaDataReducer';
 import {
-	Button,
-	Box,
-	Collapse,
-	Table,
-	TableBody,
-	TableCell,
-	TableRow,
-	Typography,
+  Button,
+  Box,
+  Collapse,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
 } from '@material-ui/core';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 import { CreatePartitionModal } from './CreatePartitionModal';
 
 interface CollapsibleTableProps {
-	openTable: boolean;
-	dataEntries: DataEntries;
+  openTable: boolean;
+  dataEntries: DataEntries;
 }
 
 export const CollapsibleTable: FC<CollapsibleTableProps> = ({
-	openTable,
-	dataEntries,
+  openTable,
+  dataEntries,
 }) => {
-	const [openPartitionModal, setOpenPartitionModal] = useState<boolean>(false);
-	return (
-		<Collapse in={openTable} timeout='auto' unmountOnExit>
-			<Box margin={3}>
-				<div className='innerTableTitle'>
-					<Typography
-						style={{ fontWeight: 'bold' }}
-						align='left'
-						variant='h6'
-						gutterBottom
-						component='div'
-					>
-						Partitions
-					</Typography>
-					<Button
-						onClick={() => setOpenPartitionModal(true)}
-						variant='text'
-						className='primaryButtons'
-					>
-						Create Partition
-					</Button>
-				</div>
+  const [openPartitionModal, setOpenPartitionModal] = useState<boolean>(false);
+  const history = useHistory();
+  return (
+    <Collapse in={openTable} timeout='auto' unmountOnExit>
+      <Box margin={3}>
+        <div className='innerTableTitle'>
+          <Typography
+            style={{ fontWeight: 'bold' }}
+            align='left'
+            variant='h6'
+            gutterBottom
+            component='div'
+          >
+            Partitions
+          </Typography>
+          <Button
+            onClick={() => setOpenPartitionModal(true)}
+            variant='text'
+            className='primaryButtons'
+          >
+            Create Partition
+          </Button>
+        </div>
 
-				<Table size='small' aria-label='partitions'>
-					{/* Table headers for Partitions */}
-					<CollapsibleTableHeaders />
+        <Table size='small' aria-label='partitions'>
+          {/* Table headers for Partitions */}
+          <CollapsibleTableHeaders />
 
-					{/* Table Body */}
-					<TableBody>
-						{dataEntries.partitionData.map((data: partitionDataEntries) => (
-							<TableRow hover={true} key={data.id}>
-								<TableCell>{data.id}</TableCell>
-								<TableCell>{data.leader.toString()}</TableCell>
-								<TableCell>{data.partitionErrorCode}</TableCell>
-								<TableCell>{data.isr}</TableCell>
-								<TableCell>{data.replicas}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-				<CreatePartitionModal
-					modalStatus={openPartitionModal}
-					setModalStatus={setOpenPartitionModal}
-					topic={dataEntries.topicName}
-					currPartitionNumber={dataEntries.partitions}
-				/>
-			</Box>
-		</Collapse>
-	);
+          {/* Table Body */}
+          <TableBody>
+            {dataEntries.partitionData.map((data: partitionDataEntries) => (
+              <TableRow hover={true} key={data.id}>
+                <TableCell>{data.id}</TableCell>
+                <TableCell>{data.leader.toString()}</TableCell>
+                <TableCell>{data.partitionErrorCode}</TableCell>
+                <TableCell>{data.isr}</TableCell>
+                <TableCell>{data.replicas}</TableCell>
+                <TableCell>
+                  <VisibilityIcon
+                    onClick={() => {
+                      console.log(`/${dataEntries.topicName}/${data.id}`);
+                      history.push(`/${dataEntries.topicName}/${data.id}`);
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <CreatePartitionModal
+          modalStatus={openPartitionModal}
+          setModalStatus={setOpenPartitionModal}
+          topic={dataEntries.topicName}
+          currPartitionNumber={dataEntries.partitions}
+        />
+      </Box>
+    </Collapse>
+  );
 };
