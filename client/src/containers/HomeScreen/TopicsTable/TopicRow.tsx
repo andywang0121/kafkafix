80cace80d3686d1a9f403de795aa9e29f472e3c1
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-08-12 19:13:47 -0400

    fix formatting

--- client/src/containers/HomeScreen/TopicsTable/TopicRow.tsx
+++ client/src/containers/HomeScreen/TopicsTable/TopicRow.tsx
@@ -8,58 +8,58 @@ import { CollapsibleTable } from './CollapsibleTable';
 import { populateData } from '../../../helperFunctions/populateData';
 
 interface TopicRowProps {
-	row: DataEntries;
+  row: DataEntries;
 }
 
 export const TopicRow: FC<TopicRowProps> = ({ row }) => {
-	const [openInnerTable, setOpenInnerTable] = useState<boolean>(false);
-	const dispatch = useDispatch();
+  const [openInnerTable, setOpenInnerTable] = useState<boolean>(false);
+  const dispatch = useDispatch();
 
-	const deleteTopicHandler = () => {
-		const topic = row.topicName;
-		console.log('delete topic topic name', topic);
-		const options = {
-			method: 'DELETE',
-			body: JSON.stringify({ topic }),
-			headers: { 'Content-Type': 'application/json' },
-		};
+  const deleteTopicHandler = () => {
+    const topic = row.topicName;
+    console.log('delete topic topic name', topic);
+    const options = {
+      method: 'DELETE',
+      body: JSON.stringify({ topic }),
+      headers: { 'Content-Type': 'application/json' },
+    };
 
-		fetch('http://localhost:3000/api/topic', options)
-			.then(data => data.json())
-			.then(data => {
-				populateData(data, dispatch);
-			})
-			.catch(e => console.log('error in deleting topic, ', e));
-	};
+    fetch('http://localhost:3000/api/topic', options)
+      .then((data) => data.json())
+      .then((data) => {
+        populateData(data, dispatch);
+      })
+      .catch((e) => console.log('error in deleting topic, ', e));
+  };
 
-	return (
-		<>
-			<TableRow className='root'>
-				<TableCell>
-					<IconButton
-						aria-label='expand row'
-						size='small'
-						onClick={() => setOpenInnerTable(!openInnerTable)}
-					>
-						{openInnerTable ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-					</IconButton>
-				</TableCell>
+  return (
+    <>
+      <TableRow className='root'>
+        <TableCell>
+          <IconButton
+            aria-label='expand row'
+            size='small'
+            onClick={() => setOpenInnerTable(!openInnerTable)}
+          >
+            {openInnerTable ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          </IconButton>
+        </TableCell>
 
-				<TableCell>{row.topicName}</TableCell>
-				<TableCell>{row.partitions}</TableCell>
-				<TableCell>
-					<Button variant='text' size='small' onClick={deleteTopicHandler}>
-						Delete
-					</Button>
-				</TableCell>
-			</TableRow>
+        <TableCell>{row.topicName}</TableCell>
+        <TableCell>{row.partitions}</TableCell>
+        <TableCell>
+          <Button variant='text' size='small' onClick={deleteTopicHandler}>
+            Delete
+          </Button>
+        </TableCell>
+      </TableRow>
 
-			{/* Create another TableRow for the partitions*/}
-			<TableRow>
-				<TableCell className='removeVerticalPadding' colSpan={5}>
-					<CollapsibleTable openTable={openInnerTable} dataEntries={row} />
-				</TableCell>
-			</TableRow>
-		</>
-	);
+      {/* Create another TableRow for the partitions*/}
+      <TableRow>
+        <TableCell className='removeVerticalPadding' colSpan={5}>
+          <CollapsibleTable openTable={openInnerTable} dataEntries={row} />
+        </TableCell>
+      </TableRow>
+    </>
+  );
 };
