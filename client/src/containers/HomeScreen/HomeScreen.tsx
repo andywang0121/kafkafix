0bafefc22d8f26c335473678dbbb636503c7fed6
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-08-12 18:30:38 -0400

    change logic to use useHistory hook instead of rendering redirect component

--- client/src/containers/HomeScreen/HomeScreen.tsx
+++ client/src/containers/HomeScreen/HomeScreen.tsx
@@ -1,22 +1,27 @@
-import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-import { RootState } from '../../state/reducers/index';
-import { StartContainer } from './StartContainer/StartContainer';
-import NavBar from './Sidepanel/NavBar';
-import { TopicsTable } from './TopicsTable/TopicsTable';
+import { RootState } from "../../state/reducers/index";
+import { StartContainer } from "./StartContainer/StartContainer";
+import NavBar from "./Sidepanel/NavBar";
+import { TopicsTable } from "./TopicsTable/TopicsTable";
+
+import "../../../stylesheets/HomeScreen.css";
 
 export const HomeScreen: FC = () => {
-	const email = useSelector((state: RootState) => state.user.email);
+  const email = useSelector((state: RootState) => state.user.email);
+  const history = useHistory();
+  if (!email) history.push("/");
 
-	return (
-		<React.Fragment>
-			<NavBar />
-			<StartContainer />
-			<TopicsTable />
-			{/* <button onClick={()=>dispatch(logoutActionCreator())}>Sign Out</button> */}
-			{!email && <Redirect to='/' />}
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <NavBar />
+      <div className="homeContainer">
+        <StartContainer />
+        <TopicsTable />
+      </div>
+      {/* <button onClick={()=>dispatch(logoutActionCreator())}>Sign Out</button> */}
+    </React.Fragment>
+  );
 };
