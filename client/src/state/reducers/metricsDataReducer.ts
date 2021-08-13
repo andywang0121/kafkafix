4b006c378c27e8db1a06ce41cc26350f1b58ae02
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-08-12 23:53:59 -0400

    set up metricsDataReducer

---
+++ client/src/state/reducers/metricsDataReducer.ts
@@ -0,0 +1,101 @@
+import { Type } from '../constants/metricsConstants';
+import { Action } from '../actions/metricsActionCreators';
+import { startTimer } from 'winston';
+
+// export interface MetricsState {
+//   chartData: ChartData | Object;
+// }
+
+// interface ChartData {
+//   labels: string[];
+//   datasets: DataSet[];
+// }
+
+// interface DataSet {
+//   label: string;
+//   data: number[];
+//   backgroundColor: string[];
+//   borderWidth: number;
+// }
+
+export type singleValData = [number?, string?];
+
+export interface quartileData {
+  '0.50': singleValData;
+  '0.75': singleValData;
+  '0.95': singleValData;
+  '0.98': singleValData;
+  '0.99': singleValData;
+  '0.999': singleValData;
+}
+
+export interface MetricsState {
+  bytesOutPerSec: singleValData[];
+  isrShrinkPerSec: singleValData[];
+  leaderElectionRateAndTimeMS: quartileData[];
+  uncleanLeaderElectionsPerSec: quartileData[];
+  activeContollerCount: singleValData;
+  offlinePartitionCount: singleValData;
+  underReplicatedPartitions: singleValData;
+  garbageCollectionCount: singleValData;
+  createTopics: singleValData[];
+  leaderAndIsr: singleValData[];
+  listGroups: singleValData[];
+  metadata: singleValData[];
+  updateMetaData: singleValData[];
+  bytesInPerSec: singleValData[];
+  fetch: singleValData[];
+  produce: singleValData[];
+  messagesPerSecond: singleValData[];
+  zkRequestLatencyMsCount: singleValData;
+  cpuSecondsTotal: singleValData;
+  zkRequestLatencyMs: quartileData[];
+  codeCache: singleValData;
+  heap: singleValData[];
+  nonheap: singleValData[];
+}
+
+const initialState: MetricsState = {
+  bytesOutPerSec: [],
+  isrShrinkPerSec: [],
+  leaderElectionRateAndTimeMS: [],
+  uncleanLeaderElectionsPerSec: [],
+  activeContollerCount: [],
+  offlinePartitionCount: [],
+  underReplicatedPartitions: [],
+  garbageCollectionCount: [],
+  createTopics: [],
+  leaderAndIsr: [],
+  listGroups: [],
+  metadata: [],
+  updateMetaData: [],
+  bytesInPerSec: [],
+  fetch: [],
+  produce: [],
+  messagesPerSecond: [],
+  zkRequestLatencyMsCount: [],
+  cpuSecondsTotal: [],
+  zkRequestLatencyMs: [],
+  codeCache: [],
+  heap: [],
+  nonheap: [],
+};
+
+export const metricsReducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case Type.APPEND:
+      return {
+        ...state,
+        [action.metric]: [...state[action.metric], action.payload],
+      };
+
+    case Type.UPDATE:
+      return {
+        ...state,
+        [action.metric]: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
