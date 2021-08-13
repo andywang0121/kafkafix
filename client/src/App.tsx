19a0c3c40a6556e2149874eda72aabe7aa2300b2
Andy Wang <andywang0121@DESKTOP-A7F0284.localdomain>
2021-08-13 01:47:07 -0400

    wrap add with StyleProvider component from material ui to use css for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import {
-	HashRouter as Router,
-	Switch,
-	Route,
-	Redirect,
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
 } from 'react-router-dom';
 
 import { HomeScreen } from './containers/HomeScreen/HomeScreen';
@@ -15,47 +15,48 @@ import { PartitionScreen } from './containers/PartitionScreen/PartitionScreen';
 import { Groups } from './containers/HomeScreen/Sidepanel/Groups';
 import { Login } from './containers/Authentication/Login';
 import { appendMessageActionCreator } from './state/actions/kafkaActionCreators';
+import { StylesProvider } from '@material-ui/core/styles';
 
 const ws = new WebSocket('ws://localhost:3000');
 ws.onopen = () => console.log('connected to websocket');
 
 export const App: FC = () => {
-	const email = useSelector((state: RootState) => state.user.email);
-	const dispatch = useDispatch();
+  const email = useSelector((state: RootState) => state.user.email);
+  const dispatch = useDispatch();
 
-	ws.onmessage = (event: any) => {
-		console.log('event data from ws', event.data);
-		const array = event.data.split('message: ');
-		const data = JSON.parse(array[1]);
-		dispatch(appendMessageActionCreator(data));
-		// if (!togglePause) setPageIndex(Math.floor(messages.length / pageSize));
-	};
-	return (
-		<>
-			<Router>
-				<Switch>
-					<Route path='/metrics' component={MetricsScreen} />
-					<Route path='/failureReports' component={FailureReportScreen} />
-					<Route
-						path='/'
-						exact
-						render={() => (email.length ? <Redirect to='/home' /> : <Login />)}
-					/>
-					<Route path='/home' exact component={HomeScreen} />
-					<Route path='/metrics' component={MetricsScreen} />
-					<Route path='/failureReports' component={FailureReportScreen} />
-					<Route path='/groups' component={Groups} />
-					<Route
-						path='/partition/:topic/:partitionID'
-						render={props => (
-							<PartitionScreen
-								topic={props.match.params.topic}
-								partitionID={props.match.params.partitionID}
-							/>
-						)}
-					/>
-				</Switch>
-			</Router>
-		</>
-	);
+  ws.onmessage = (event: any) => {
+    console.log('event data from ws', event.data);
+    const array = event.data.split('message: ');
+    const data = JSON.parse(array[1]);
+    dispatch(appendMessageActionCreator(data));
+    // if (!togglePause) setPageIndex(Math.floor(messages.length / pageSize));
+  };
+  return (
+    <StylesProvider injectFirst>
+      <Router>
+        <Switch>
+          <Route path='/metrics' component={MetricsScreen} />
+          <Route path='/failureReports' component={FailureReportScreen} />
+          <Route
+            path='/'
+            exact
+            render={() => (email.length ? <Redirect to='/home' /> : <Login />)}
+          />
+          <Route path='/home' exact component={HomeScreen} />
+          <Route path='/metrics' component={MetricsScreen} />
+          <Route path='/failureReports' component={FailureReportScreen} />
+          <Route path='/groups' component={Groups} />
+          <Route
+            path='/partition/:topic/:partitionID'
+            render={(props) => (
+              <PartitionScreen
+                topic={props.match.params.topic}
+                partitionID={props.match.params.partitionID}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
 };
